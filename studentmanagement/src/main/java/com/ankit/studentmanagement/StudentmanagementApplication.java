1f022e9df034c3fc096ad652e022ad6dd5e586cf
io6-ankit <156927710+io6-ankit@users.noreply.github.com>
2026-07-03 20:22:51 +0530

    springboot-management-API

--- studentmanagement/src/main/java/com/ankit/studentmanagement/StudentmanagementApplication.java
+++ studentmanagement/src/main/java/com/ankit/studentmanagement/StudentmanagementApplication.java
@@ -16,8 +16,8 @@ public class StudentmanagementApplication {
     // Yeh bean aapki properties file ko override karke direct Atlas se connect karwayegi
     @Bean
     public MongoClient mongoClient() {
-        // Apni real Atlas connection string yahan paste karein (password badalna mat bhoolna)
-        String atlasUri = "mongodb+srv://mauryaankit993535_db_user:vq18aS2YdOnA8Q5B@cluster0.8m7b35j.mongodb.net/";
+        // यह सिस्टम से MONGO_URI की वैल्यू उठाएगा
+        String atlasUri = System.getenv("MONGO_URI");
         return MongoClients.create(atlasUri);
     }
 }
